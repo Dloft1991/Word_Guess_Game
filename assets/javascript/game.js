f2d016d0f774e8f16f588a20a2557642843c9f81
Dloft <derek.lofton1@gmail.com>
2018-12-11 12:56:31 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,39 @@
-const computerChoice = ['ironman', 'captainamerica', 'thor', 'thanos', 'redskull', 'antman', 'spiderman', 'blackpanther', 'captainmarvel', 'blackwidow', 'loki', 'hulk'];
+const words = ['ironman', 'captainamerica', 'thor', 'thanos', 'redskull', 'antman', 'spiderman', 'blackpanther', 'captainmarvel', 'blackwidow', 'loki', 'hulk'];
 const game = document.querySelector('main');
 
-let maxGuesses = 10,
-    userGuess = [],
-    rightGuess = [],
-    guessesLeft = 0;
-let currentWord;
+let hiddenWord = [],
+    guessLeft = 10,
+    guessedLetters = [];
 
 
 document.onkeyup = function(event) {
     const userGuess = event.key;
 
-    const computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-
-    guessedLetters = [];
-    rightGuess = [];
-
-    for (var i =0; i < computerChoice[currentWord].length; i++) {
-        rightGuess.push('_');
-    }
+    const answer = words[Math.floor(Math.random() * words.length)];
 
     
-    game.innerHTML = `
-        <p>You Choose: ${userGuess}<p>
-        <P>Computer Chose: ${computerGuess}<p>
-        <p>wins: ${wins}<p>
-        <p>losses: ${losses}<p>
-        <p>Guesses Left: ${maxGuesses}<p>
-        <p>Your Picks: ${wrongPick}<p>
-         `
+    document.getElementById("guessLeft").innerText = "10";
+    document.getElementById("guessedLetters").innerText = " ";
+
+    for (var i = 0; i < words.length; i++) {
+        document.getElementById("hiddenWord").innerText = "_";
+    }
+    if (userGuess === words[i]) {
+        guessLeft--;
+        hiddenWord.push("_");
+    } else {
+        guessLeft--;
+    }
+
+
+
+
+    
+    // game.innerHTML = `
+    //     <p>You Choose: ${userGuess}<p>
+    //     <P>Computer Chose: ${computerGuess}<p>
+    //     <p>Word: ${rightGuess}<p>
+    //     <p>Guesses Left: ${maxGuesses}<p>
+    //      `
 
 }
